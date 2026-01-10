876fa2d7787184dfdbed2d052d1c7065ffa10c15
Plasma Strike <plasmastrike@voiddragon.me>
2026-01-10 07:49:17 -0700

    fixv3

--- packages.el
+++ packages.el
@@ -70,10 +70,12 @@
 (package! emacs-reader)
 (package! calfw)
 ;; (package! calfw-blocks)
-(package! calfw-blocks
-  :recipe (:host github :repo haji-ali/calfw-blocks
-           :pin "87937b7c7523b6d314bf9e21310924c94f182954"))
+;; (package! calfw-blocks
+;;   :recipe (:host github :repo haji-ali/calfw-blocks
+;;            :pin "87937b7c7523b6d314bf9e21310924c94f182954"))
 
+(package! calfw-blocks
+  :recipe (:pin  "https://https://github.com/haji-ali/calfw-blocks" "87937b7c7523b6d314bf9e21310924c94f182954"))
 ;; (package! reader  ; or emacs-reader
 ;;   :recipe (:host codeberg :repo divyaranjan/emacs-reader))
 
